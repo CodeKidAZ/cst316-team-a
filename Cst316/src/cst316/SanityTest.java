aab9600414c75229c9f51267ba4485d718faae38
Michael Howell <michael@notriddle.com>
2015-02-16 13:14:31 -0700

    Add a test target to Ant.
    Notes:
    
      * Why doesn't `cst316.PlayerTest` delete `dan.json` at the end?
      * Why doesn't `cst316.PrintMarketingTest` pass the first time?

---
+++ Cst316/src/cst316/SanityTest.java
@@ -0,0 +1,18 @@
+package cst316;
+
+import java.util.ArrayList;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SanityTest {
+	
+    @Before
+    public void setUp() {
+        //
+    }
+	@Test
+	public void testAssets() {
+        assertEquals(true, true);
+	}
+}
