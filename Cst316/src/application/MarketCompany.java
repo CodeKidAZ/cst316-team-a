e7739838d65e637cf44e3cbe9ab9d2a3e85da83d
WesCoomber <wcoomber@asu.edu>
2015-03-31 22:00:49 -0700

    Added the functionality of the MarketShareMeasure task logic implemented using java arraylists.

--- Cst316/src/application/MarketCompany.java
+++ Cst316/src/application/MarketCompany.java
@@ -10,6 +10,16 @@ package application;
 public class MarketCompany {
 
 	/**
+	 * @param name
+	 * @param marketPower
+	 */
+	public MarketCompany(String name, double marketPower) {
+		super();
+		this.name = name;
+		this.marketPower = marketPower;
+	}
+
+	/**
 	 * @param args
 	 */
 	private String name;
@@ -18,6 +28,34 @@ public class MarketCompany {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the marketPower
+	 */
+	public double getMarketPower() {
+		return marketPower;
+	}
+
+	/**
+	 * @param marketPower the marketPower to set
+	 */
+	public void setMarketPower(double marketPower) {
+		this.marketPower = marketPower;
 	}
 
 }
--- Cst316/src/application/MarketShareMeasure.java
+++ Cst316/src/application/MarketShareMeasure.java
@@ -3,6 +3,8 @@
  */
 package application;
 
+import java.util.ArrayList; 
+
 /**
  * @author Wesley Local
  *
@@ -12,8 +14,11 @@ public class MarketShareMeasure {
 	/**
 	 * @param args
 	 */
+
 	
-	int[] myIntArray = new int[]{1,2,3};
+	static MarketCompany mcSample1 = new MarketCompany("Wendys", 15.5);
+	static MarketCompany mcSample2 = new MarketCompany("BurgerKing", 35.9);
+	static MarketCompany mcSample3 = new MarketCompany("McDonalds", 75.9);
 	
 	double getMarketCapital(double shares, double price){
 		double marketCapital = (shares*price);
@@ -42,32 +47,46 @@ public class MarketShareMeasure {
 		return capTerm;
 	}
 	
-	double getMarketPercent(double marketCapital){
-		String capTerm = "";
+//
+	static double getMarketPercent(ArrayList<MarketCompany> mcIn, String compName){
+		double targetPower = -1;
+		double totalPower = -2;
+		double ret = -3;
 		
-		marketCapital = marketCapital/1000000000; //converts market capital value into a billion count, so eg. 500000000 = .5 billion
-		//This is done so that way I can use smaller compare values in the if statements that do not go outside of variable range
-		
-		// checks if company's marketCapitalzation value is  < 2 billion
-		if (marketCapital < 2){
-			capTerm = "Low Cap";
-		}
-		// checks if company's marketCapitalzation value is  > 2 billion and < 10 billion
-		else if (marketCapital > 2 && marketCapital < 10){
-			capTerm = "Mid Cap";
-		}
-		// if it does not fulfill the previous two if statements, then the passed in value must be > 10 billion
-		else{
-			capTerm = "High Term Cap";
+		for (int z = 0; z < mcIn.size(); z ++){
+			  if(compName.equals(mcIn.get(z).getName()))
+			  {
+				  targetPower = (mcIn.get(z).getMarketPower());
+			  }
+			  totalPower = totalPower+(mcIn.get(z).getMarketPower());
 		}
 		
-		return capTerm;
+		ret = (targetPower / totalPower) * 100;
+		return ret;
+	
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+
+
+		ArrayList<MarketCompany> companyList = new ArrayList<MarketCompany>();
+		companyList.add(mcSample1);
+		companyList.add(mcSample2);
+		companyList.add(mcSample3);
 		
+		double d = getMarketPercent(companyList, "Wendys");
+		
+		System.out.println("The market percent of Wendy's is: " + d);
+		
+		d = getMarketPercent(companyList, "BurgerKing");
+		
+		System.out.println("The market percent of BurgerKing is: " + d);
+		
+		d = getMarketPercent(companyList, "McDonalds");
+		
+		System.out.println("The market percent of McDonalds's is: " + d);
 
 	}
 
