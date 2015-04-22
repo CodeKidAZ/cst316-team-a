fd907eafc9d1639ca498d45d0fb9af27948f36a5
WesCoomber <wcoomber@asu.edu>
2015-04-22 12:44:39 -0700

    The getMarketPercent() method in MarketShareMeasure did a sub par job of exception handling. Created a new exception called MyMarketingPercentException to catch each specific type of checked exception possible within the function block. This prints out a message indicating the type of exception caught (e.g. "Caught Array index out of bounds exception."), and then re-throws this new exception.

---
+++ Cst316/src/cst316/MyMarketPercentException.java
@@ -0,0 +1,18 @@
+package cst316;
+/**
+ * 
+ */
+
+/**
+ * @author Wesley Local
+ *
+ */
+public class MyMarketPercentException extends Exception
+{
+    public MyMarketPercentException() {}
+
+    public MyMarketPercentException(String message)
+    {
+       super(message);
+    }
+}
